917ed47c93fd198bdc256798afd4536d75ae244f
Amir Mostofaa <mostafacox1@gmail.com>
2020-10-01 13:03:21 +0600

    updated

---
+++ assignment.js
@@ -0,0 +1,16 @@
+assignment.js
+1. feetToMile
+2. woodCalculator
+3. brickCalculator
+4. tinyFriend
+
+
+// feet to mile conversion
+// 1mile = 5280 feet
+
+function feetToMile(feet) {
+    return feet / 5280;
+}
+var result = feetToMile(10820);
+console.log(result)
+
